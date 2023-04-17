aa9cf961e149d7f0f1e2254e0452220c12fb5a4e
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-17 21:29:36 +0500

    Initial Commit

---
+++ GuessNumber/GuessNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberApp.swift
+//  GuessNumber
+//
+//  Created by sss on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
